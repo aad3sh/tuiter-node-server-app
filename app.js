1c306fac408cb4b8c4a24756175ca736252bf502
aad3sh <mallya.aa@northeastern.edu>
2022-11-28 19:27:01 -0500

    a9 Mongo fa22

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ import UserController
     from "./controllers/users/users-controller.js"
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
+import mongoose from 'mongoose'
 const app = express()
 app.use(cors())
 app.use(express.json());
+const CONNECTION_STRING = 'mongodb+srv://AadeshMallya:gremlin27@cluster0.iqh7ibu.mongodb.net/tuiter?retryWrites=true&w=majority'
+    || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
 TuitsController(app);
 HelloController(app)
 UserController(app)
